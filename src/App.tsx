ad852c237cd3d6c235bd145f5a6743e47f6ddf51
namassist <chairulanmm@gmail.com>
2024-09-25 21:20:19 +0700

    [fix] remove some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { router } from "@/routes";
 import { RouterProvider } from "react-router-dom";
 
-import { Navbar } from "@/components/navbar";
-import { Footer } from "@/components/footer";
-
 const App = () => {
   return (
     <main className="font-nunito">
-      <Navbar />
       <RouterProvider router={router} />
-      <Footer />
     </main>
   );
 };
