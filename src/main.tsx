f0e09e522db7ea818c9a1e776f182f1d4854e5fe
namassist <chairulanmm@gmail.com>
2024-09-25 21:35:03 +0700

    [feat] add app provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./globals.css";
+import AppProvider from "./context/app-context.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </QueryClientProvider>
 );
