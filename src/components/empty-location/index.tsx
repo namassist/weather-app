595c547dc577fa4d082e22942c7931e74ba337de
namassist <chairulanmm@gmail.com>
2024-09-26 13:52:43 +0700

    [fix] responsive design emptLocation component

--- src/components/empty-location/index.tsx
+++ src/components/empty-location/index.tsx
@@ -7,14 +7,14 @@ export const EmptyLocation = () => {
       <div className="w-full flex justify-center">
         <img src={Logo} alt="logo app" className="h-[32px]" />
       </div>
-      <div className="mt-40 flex flex-col justify-center items-center">
+      <div className="mt-40 flex flex-col justify-center items-center text-center sm:text-left">
         <h1 className="text-heading-lg text-gray-100">
           Welcome to <span className="text-blue-light">TypeWeather</span>
         </h1>
         <p className="text-gray-200 text-lg">
           Choose a location to view the weather forecast
         </p>
-        <div className="w-[512px] mt-10">
+        <div className="w-full lg:w-[512px] mt-10">
           <SearchForm />
         </div>
       </div>
